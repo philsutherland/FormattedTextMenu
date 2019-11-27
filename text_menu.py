2e8f42c718d8429ffbe73acc1951e01226045419
Phil Sutherland <psuth026@uottawa.ca>
2019-11-27 13:46:18 -0500

    Optimized code in text menu object

--- text_menu.py
+++ text_menu.py
@@ -9,46 +9,48 @@ class TextMenu:
         self.options = options
 
     def navigate(self, previous_menu, returning=False):
+        # If the return button has not been selected, store the previous menu location
         if not returning:
             if previous_menu is None:
                 self.previous_menu = self
             else:
                 self.previous_menu = previous_menu
 
-        menu_options = list(self.options)
+        # Print out title of the menu
         print(TMF.sub_title(self.title))
 
+        # If there is a menu message, print it out
         if self.message:
             print(TMF.body_content(self.message))
 
-        print(TMF.options_menu(menu_options))
+        # Print the menu options out as a list
+        print(TMF.options_menu(list(self.options)))
 
-        nav_map = {}
-        x = 0
-        for option in menu_options:
-            x += 1
-            nav_map[str(x)] = option
-
+        # If a value other than an integer is entered, display error and show menu again
         try:
             selection = int(input("Select Option: "))
+            selection = selection - 1
         except(BaseException):
             TMF.clear()
-            print(TMF.error_title("Invalid Selection!"))
+            print(TMF.error_title("Please only enter an integer!"))
             self.navigate(self, True)
 
-        if str(selection) in nav_map:
-            if self.options[nav_map[str(selection)]] == "return":
+        # Convert menu option values into a list for comparison to user selected option
+        nav_map = list(self.options.values())
+
+        # If an incorrect option is chosen, display error and show menu again
+        if 0 <= selection < len(nav_map):
+            if nav_map[selection] == "return":
                 TMF.clear()
                 self.previous_menu.navigate(self, True)
-                pass
-            elif self.options[nav_map[str(selection)]] == "exit":
+            elif nav_map[selection] == "exit":
                 TMF.clear()
                 print(TMF.main_title("Goodbye!"))
                 exit(0)
             else:
                 TMF.clear()
-                self.options[nav_map[str(selection)]](self)
+                nav_map[selection](self)
         else:
             TMF.clear()
-            print(TMF.error_title("Invalid Selection!"))
+            print(TMF.error_title("That option does not exist!"))
             self.navigate(self, True)
