f44941bdb4aded33cadd6140c4fcc4a3260e9f6b
Phil Sutherland <psuth026@uottawa.ca>
2019-11-27 11:40:01 -0500

    Text menu formatting done

---
+++ text_menu_formatting.py
@@ -0,0 +1,54 @@
+import os
+
+# Change this constant to increase/decrease maximum printout length
+MENU_LEN = 63
+
+
+class FormattedTextMenu:
+    @staticmethod
+    def main_title(title):
+        outer_line = "|" + "*" * (MENU_LEN - 2) + "|"
+        middle_line = "|" + \
+            ("{:^" + str(MENU_LEN - 2) + "}").format(title) + "|"
+        return outer_line + "\n" + middle_line + "\n" + outer_line
+
+    @staticmethod
+    def sub_title(title):
+        outer_line = "|" + "=" * (MENU_LEN - 2) + "|"
+        middle_line = "|" + \
+            ("{:^" + str(MENU_LEN - 2) + "}").format(title) + "|"
+        return outer_line + "\n" + middle_line + "\n" + outer_line
+
+    @staticmethod
+    def options_menu(options):
+        outer_line = "|" + "-" * (MENU_LEN - 2) + "|"
+        options_text = "|" + \
+            ("{:<" + str(MENU_LEN - 2) + "}").format(" Options:") + "|\n"
+
+        for option in options:
+            options_text += "|" + ("{:<" + str(MENU_LEN - 2) + "}").format((" " * 6)
+                                                                           + str(options.index(option) + 1) + ". " + option) + "|\n"
+        return options_text + outer_line
+
+    @staticmethod
+    def body_content(content):
+        outer_line = "|" + "-" * (MENU_LEN - 2) + "|"
+        string_list = content.split('\n')
+        content_text = ""
+
+        for string in string_list:
+            content_text += "|" + \
+                ("{:<" + str(MENU_LEN - 2) + "}").format(string) + "|\n"
+        return content_text + outer_line
+
+    @staticmethod
+    def error_title(title):
+        outer_line = "|" + "#" * (MENU_LEN - 2) + "|"
+        middle_line = "|" + \
+            ("{:^" + str(MENU_LEN - 2) + "}").format(title) + "|"
+        return outer_line + "\n" + middle_line + "\n" + outer_line
+
+    @staticmethod
+    def clear():
+        os.system("cls")
+        print("")
